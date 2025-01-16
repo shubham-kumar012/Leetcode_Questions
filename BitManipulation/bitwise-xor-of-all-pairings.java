247e37c06d0f318f2a484d16a764a2b816dc1325
Shubham Kumar <shubhampal7083@gmail.com>
2025-01-16 22:24:48 +0530

    Create bitwise-xor-of-all-pairings.java
    Q.2425 Medium

---
+++ BitManipulation/bitwise-xor-of-all-pairings.java
@@ -0,0 +1,34 @@
+// Q.2425 Medium
+// Leetcode -> https://leetcode.com/problems/bitwise-xor-of-all-pairings/description/
+
+// T.C. -> 
+// S.C. ->
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int xor = 0;
+        // a ^ a = 0
+        // so if any nums1 length is even and nums2 length are odd, so nums2 all element occur even time those result will 0, so we consider only xor of nums1. and that will be our final ans
+        // so just like we create this conditions
+        if(m % 2 == 0 && n % 2 != 0) {// if nums1 is even and nums2 is not even
+            for(int i=0; i<m; i++) {
+                xor = xor ^ nums1[i];
+            }
+        } else if(m % 2 != 0 && n % 2 == 0) {// if nums1 is odd and nums2 is even
+            for(int i=0; i<n; i++) {
+                xor = xor ^ nums2[i];
+            }
+        } else if(m % 2 != 0 && n %2 != 0) {// if nums1 and nums2 both are odd
+            for(int i=0; i<m; i++) {
+                xor = xor ^ nums1[i];
+            }
+
+            for(int i=0; i<n; i++) {
+                xor = xor ^ nums2[i];
+            }
+        }
+
+        return xor;
+    }
+}
