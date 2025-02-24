5b948a5509454361af6209b5a3eb8db61348f477
Shubham Kumar <shubhampal7083@gmail.com>
2025-02-24 13:18:05 +0530

    Create path-sum.java
    Q.112 Easy

---
+++ Tree/path-sum.java
@@ -0,0 +1,29 @@
+// Q.112 Easy
+// Leetcode - https://leetcode.com/problems/path-sum/description/
+
+// T.C. -> 
+// S.C. ->
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false; // if root are null so return false
+
+        return solve(root, targetSum, 0);
+    }
+
+    private boolean solve(TreeNode root, int targetSum, int currSum) {
+        if(root == null) {
+            return false;
+        }
+
+        currSum += root.val;
+
+        if(root.left == null && root.right == null) {
+            return targetSum == currSum;
+        }
+
+        boolean left = solve(root.left, targetSum, currSum);
+        boolean right = solve(root.right, targetSum, currSum);
+
+        return left || right;
+    }
+}
