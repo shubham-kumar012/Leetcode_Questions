842fb88ce54025c84118f68cfb0bf466e0a976a0
Shubham Kumar <shubhampal7083@gmail.com>
2026-03-05 00:12:38 +0530

    Refactor numSpecial method for special positions
    Updated time complexity to O(m * n * (m+n)) and space complexity to O(1). Refactored the logic to check for special positions in a binary matrix.

--- Arrays/special-positions-in-a-binary-matrix.java
+++ Arrays/special-positions-in-a-binary-matrix.java
@@ -1,26 +1,36 @@
-// Time Complexity - O(m * n)
-// Space Complexity - O(m + n)
+// Time Complexity - O(m * n * (m+n))
+// Space Complexity - O(1)
 // Leetcode - https://leetcode.com/problems/special-positions-in-a-binary-matrix/
 
 class Solution {
     public int numSpecial(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
-        int[] row = new int[m];
-        int[] col = new int[n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1) {
-                    row[i]++;
-                    col[j]++;
-                }
-            }
-        }
-
+        int m = mat.length; // rows
+        int n = mat[0].length; // cols
         int ans = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
-                    ans++;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(mat[i][j] == 1) {
+                    boolean flag = true; // track if position is special
+
+                    for(int k=0; k<m; k++) { // for checking whole single column
+                        if(k == i) continue;
+                        if(mat[k][j] == 1) {
+                            flag = false;
+                            break;
+                        } 
+                    }
+
+                    for(int k=0; k<n; k++) { // for checking whole single row
+                        if(k == j) continue;
+                        else if(!flag) break;
+                        else if(mat[i][k] == 1) {
+                            flag = false;
+                            break;
+                        }
+                    }
+
+                    if(flag) ans++;
                 }
             }
         }
