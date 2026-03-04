537d73b7bc9067f4f1e7412eb98ee8f80a271a3c
Shubham Kumar <shubhampal7083@gmail.com>
2026-03-04 23:45:39 +0530

    Create special-positions-in-a-binary-matrix.java
    1582. Easy

---
+++ Arrays/special-positions-in-a-binary-matrix.java
@@ -0,0 +1,30 @@
+// Time Complexity - O(m * n)
+// Space Complexity - O(m + n)
+// Leetcode - https://leetcode.com/problems/special-positions-in-a-binary-matrix/
+
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
