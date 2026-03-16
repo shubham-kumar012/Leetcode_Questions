dd2a8be953dfb9aab5ca9e98bb3a592073ea5229
Shubham Kumar <shubhampal7083@gmail.com>
2026-03-16 22:55:40 +0530

    Create find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java

---
+++ Tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -0,0 +1,18 @@
+// Leetcode - https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+
+class Solution {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if(original == null || cloned == null) {
+            return null;
+        }
+        if(original.val == target.val && cloned.val == target.val) {
+            return cloned;
+        }
+
+        TreeNode node1 = getTargetCopy(original.left, cloned.left, target);
+        TreeNode node2 = getTargetCopy(original.right, cloned.right, target);
+
+        if(node1 == null) return node2;
+        return node1;
+    }
+}
